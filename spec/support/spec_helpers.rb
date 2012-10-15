399bd4d966d89f258a88d9557df3e992d31343f5
Chavez <matthew.chavez@authoritylabs.com>
2012-10-14 22:03:23 -0700

    Remove old Locales constants.

--- spec/support/spec_helpers.rb
+++ spec/support/spec_helpers.rb
@@ -2,6 +2,6 @@ def is_ruby_19?
   RUBY_VERSION == '1.9.1' or RUBY_VERSION == '1.9.2'
 end
 
-Encoding.default_internal = Encoding.default_external = "ASCII-8BIT" if is_ruby_19?
+# Encoding.default_internal = Encoding.default_external = "ASCII-8BIT" if is_ruby_19?
 
 TEST_KEY = '123456htva2421eda89h'
